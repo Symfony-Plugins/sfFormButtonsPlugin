d0d58d590813afeb4c1ec9085916f4fb36261eae
BigBadBassMan <BigBadBassMan@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-09-11 04:23:17 -0500

    added submit_button_to_remote() function using an AJAX-Request-button to submit a form
    git-svn-id: http://svn.symfony-project.com/plugins/sfFormButtonsPlugin/trunk@21892 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/FormButtonsHelper.php
+++ lib/helper/FormButtonsHelper.php
@@ -55,7 +55,7 @@ function button_tag($value, $options = array())
       $options['name'] = _button_tag_name($value);
     }
   }
-  
+
   return content_tag('button', $value, array_merge(array('type' => 'button'), _convert_options_to_javascript(_convert_options($options))));
 }
 
@@ -69,22 +69,22 @@ function button_tag($value, $options = array())
 function button_to_js($value, $function, $html_options = array())
 {
   $html_options = _parse_attributes($html_options);
-  
+
   $html_options['type']    = 'button';
   $html_options['onclick'] = $function.'; return false;';
-  
+
   if (!isset($html_options['name']) || !$html_options['name']) {
     if (false === strpos($value, '<img '))
     {
       $html_options['name'] = _button_tag_name($value);
     }
   }
-  
+
   return content_tag('button', $value, $html_options);
 }
 
 /**
- * Creates an <button> button tag of the given name pointing to a routed URL
+ * Creates an <button>button</button> tag of the given name pointing to a routed URL
  * based on the module/action passed as argument and the routing configuration.
  *
  * <b>Options:</b>
@@ -104,20 +104,20 @@ function button_to_js($value, $function, $html_options = array())
  * @param  string $value         value of the button (used in "name" attribute if not set)
  * @param  string $internal_uri  'module/action' or '@rule' of the action
  * @param  array  $options       additional HTML compliant <button> tag parameters
- * @return string XHTML compliant <button> tag
+ * @return string XHTML compliant <button>button tag</button>
  * @see    url_for, link_to
  */
 function button_to_url($value, $internal_uri, $options = array())
 {
   $html_options = _parse_attributes($options);
-  
+
   if (!isset($html_options['name']) || !$html_options['name']) {
     if (false === strpos($value, '<img '))
     {
       $html_options['name'] = _button_tag_name($value);
     }
   }
-  
+
   if (isset($html_options['post']) && $html_options['post']) {
     if (isset($html_options['popup'])) {
       throw new sfConfigurationException('You can\'t use "popup" and "post" together.');
@@ -125,10 +125,10 @@ function button_to_url($value, $internal_uri, $options = array())
     $html_options['type'] = 'submit';
     unset($html_options['post']);
     $html_options = _convert_options_to_javascript($html_options);
-    
+
     return form_tag($internal_uri, array('method' => 'post', 'class' => 'button_to')).content_tag('button', $value, $html_options).'</form>';
   }
-  
+
   $url = url_for($internal_uri);
   if (isset($html_options['query_string'])) {
     $url = $url.'?'.$html_options['query_string'];
@@ -149,7 +149,7 @@ function button_to_url($value, $internal_uri, $options = array())
     $html_options['onclick'] = "document.location.href=".$url.";";
     $html_options = _convert_options_to_javascript($html_options);
   }
-  
+
   return content_tag('button', $value, $html_options);
 }
 
@@ -157,4 +157,43 @@ function _button_tag_name($value)
 {
   return strtolower(preg_replace('/\s+/', '_', $value)).'_btn';
 }
+
+/**
+ *  Returns a <button>button tag</button> that will submit form using XMLHttpRequest in the background instead of regular
+ *  reloading POST arrangement. The '$options' argument is the same as in 'form_remote_tag()'.
+ *
+ *  <b>Example:</b>
+ *  <code>
+ *  <?php echo submit_button_to_remote('name', 'value' array(
+ *    'url'      => '@tag_add',
+ *    'update'   => 'question_tags',
+ *    'loading'  => "Element.show('indicator'); \$('tag').value = ''",
+ *    'complete' => "Element.hide('indicator');".visual_effect('highlight', 'question_tags'),
+ *  )) ?>
+ *  </code>
+ *  @uses JavascriptHelper.php
+ *  @see  submit_to_remote, submit_image_to_remote
+ *
+ *  @param  string  $content  content of the button, can be pure string, <img /> image tags or any allowed nested tags for <button></button>
+ *  @param  string  $value    value of the button
+ *  @param  array   options   above JavaScript-related configuration options as well as HTML-compliant additional attributes
+ *  @return string  XHTML-compliant <button>button tag</button> triggering an background AJAX-submit og the form
+ */
+function submit_button_to_remote($content, $value = 'Save', $options = array())
+{
+  $options = _parse_attributes($options);
+
+  if (!isset($options['with']))
+  {
+    $options['with'] = 'Form.serialize(this.form)';
+  }
+
+  $options['type'] = 'submit';
+  $options['onclick'] = remote_function($options).' return false;';
+  $options['name'] = 'submit';
+  $options['value'] = $value;
+
+  return content_tag('button', $content, _convert_options_to_javascript(_convert_options($options)));
+}
+
 ?>
